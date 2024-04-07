75504ee14b866f823f72c8606c781e2f4f73f6e1
TacB0sS <adam.vdk@nu-art-software.com>
2024-04-07 14:26:31 +0300

    test templater

--- .jenkins/tests/test-templater.groovy
+++ .jenkins/tests/test-templater.groovy
@@ -5,12 +5,12 @@ import com.nu.art.utils.StringTemplateReplacer
 pipeline {
 	agent any
 
-	stages {
-		stage('test templater') {
+	script {
+		stages {
+			stage('test templater') {
 //			env.PARAM = "lovely"
 ////				env.PLACEHOLDER = "zevel"
 //			env.TEST1 = "friendly"
-			script {
 				StringTemplateReplacer.replace("./.jenkins/tests/test-template.txt", "./.jenkins/tests/output.txt")
 				sh 'cat "./.jenkins/tests/output.txt"'
 			}
