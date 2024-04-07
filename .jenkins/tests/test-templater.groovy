237e80b9261c03f8800355c8f57af61a5868746d
TacB0sS <adam.vdk@nu-art-software.com>
2024-04-07 14:16:46 +0300

    test templater

--- .jenkins/tests/test-templater.groovy
+++ .jenkins/tests/test-templater.groovy
@@ -7,13 +7,11 @@ pipeline {
 
 	stages {
 		stage('test templater') {
-			steps {
-				env.PARAM = "lovely"
+			env.PARAM = "lovely"
 //				env.PLACEHOLDER = "zevel"
-				env.TEST1 = "friendly"
-				StringTemplateReplacer.replace("./.jenkins/tests/test-template.txt", "./.jenkins/tests/output.txt")
-				sh 'cat "./.jenkins/tests/output.txt"'
-			}
+			env.TEST1 = "friendly"
+			StringTemplateReplacer.replace("./.jenkins/tests/test-template.txt", "./.jenkins/tests/output.txt")
+			sh 'cat "./.jenkins/tests/output.txt"'
 		}
 	}
 }
