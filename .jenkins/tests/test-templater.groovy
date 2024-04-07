6f57802593931eab30b76a84146f6018c31db7a8
TacB0sS <adam.vdk@nu-art-software.com>
2024-04-07 14:40:55 +0300

    test templater

--- .jenkins/tests/test-templater.groovy
+++ .jenkins/tests/test-templater.groovy
@@ -1,16 +1,26 @@
 @Library('dev-tools')
-
+import com.nu.art.pipeline.workflow.Workflow
 import com.nu.art.utils.StringTemplateReplacer
+import com.nu.art.pipeline.workflow.BasePipeline
+
+class TemplaterPipeline
+	extends BasePipeline<TemplaterPipeline> {
+
+	TemplaterPipeline() {
+		super("Test Templater")
+	}
+
+	@Override
+	protected void init() {
+	}
+
+	void pipeline() {
+		StringTemplateReplacer.replace("./.jenkins/tests/test-template.txt", "./.jenkins/tests/output.txt")
+		_sh 'cat "./.jenkins/tests/output.txt"'
+	}
+}
 
 node() {
-	stages {
-		stage('test templater') {
-//			env.PARAM = "lovely"
-////				env.PLACEHOLDER = "zevel"
-//			env.TEST1 = "friendly"
-			StringTemplateReplacer.replace("./.jenkins/tests/test-template.txt", "./.jenkins/tests/output.txt")
-			sh 'cat "./.jenkins/tests/output.txt"'
-		}
-	}
+	Workflow.createWorkflow(Pipeline.class, this)
 }
 
