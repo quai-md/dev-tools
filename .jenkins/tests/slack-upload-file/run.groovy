85c1fbe57bc6a2a4066d7e26a068a5aa32752138
TacB0sS <adam.vdk@nu-art-software.com>
2024-04-15 16:48:30 +0300

    upload file

--- .jenkins/tests/slack-upload-file/run.groovy
+++ .jenkins/tests/slack-upload-file/run.groovy
@@ -26,7 +26,7 @@ class PipelineTest_SlackUploadFile
 		addStage("upload file", {
 
 			getModule(SlackModule.class).enableNotifications()
-			getModule(SlackModule.class).sendFile("dummy-file.txt", "pipeline-temp")
+			getModule(SlackModule.class).sendFile("**/dummy-file.txt", "pipeline-temp")
 
 			getModule(SlackModule.class).disable()
 		})
