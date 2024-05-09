4813cbcd8ce5e5ba97913f9cea95db81cb0fb016
Matan Koby <matan.k@quai.md>
2024-05-09 14:25:59 +0300

    Added a logger test

---
+++ .jenkins/tests/write-console-logs/run.groovy
@@ -0,0 +1,28 @@
+@Library('dev-tools@prod')
+
+import com.nu.art.pipeline.workflow.BasePipeline
+import com.nu.art.pipeline.workflow.Workflow
+
+class PipelineTest_WriteLogs
+	extends BasePipeline<PipelineTest_WriteLogs> {
+
+	PipelineTest_WriteLogs() {
+		super("Write Logs")
+	}
+
+	@Override
+	protected void init() {}
+
+	@Override
+	void pipeline() {
+        _sh("""
+        			echo "Echoed Hello World"
+        	""")
+        this.logDebug("Logged Hello World")
+
+	}
+}
+
+node() {
+	Workflow.createWorkflow(PipelineTest_WriteLogs.class, this)
+}
