5dda1de7b427646c78216fe49d09a872d1a489c4
TacB0sS <adam.vdk@nu-art-software.com>
2024-05-21 12:26:14 +0300

    test ts child process

--- .jenkins/tests/test-ts-child-process/run.groovy
+++ .jenkins/tests/test-ts-child-process/run.groovy
@@ -30,8 +30,10 @@ class PipelineTest_ChildProcessTS
 		_sh("""echo 18.15.0>.nvmrc""") // set nvm version
 		_sh("""curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh" | bash""") // install nvm
 		_sh("""
-				export NVM_DIR="\$HOME/.nvm"
-				[ -s "\$NVM_DIR/nvm.sh" ] && \\. "\$NVM_DIR/nvm.sh"  # This loads nvm """)
+				export NVM_DIR="\$HOME/.nvm > install-nvm.sh"
+				[ -s "\$NVM_DIR/nvm.sh" ] && \\. "\$NVM_DIR/nvm.sh"  # This loads nvm >> install-nvm.sh
+				bash install-nvm.sh
+""")
 		_sh("""nvm use""") // nvm rc
 		_sh("""npm i -g ts-node@latest""") // install ts-node global
 		_sh("""ts-node ./test.ts""") // install ts-node global
