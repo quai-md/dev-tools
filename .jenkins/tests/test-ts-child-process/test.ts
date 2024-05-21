f904a32dbfa3ec67ff101fe34b1fb2c655cedb54
TacB0sS <adam.vdk@nu-art-software.com>
2024-05-21 12:41:35 +0300

    test ts child process

--- .jenkins/tests/test-ts-child-process/test.ts
+++ .jenkins/tests/test-ts-child-process/test.ts
@@ -1,6 +1,12 @@
+import {exec} from 'child_process';
+
+
 console.log('SUCCESS!!');
 
-// exec('echo YEY!!');
-// exec('export NVM_DIR="$HOME/.nvm"\n' +
-// 	'[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm\n' +
-// 	'[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion', {shell: '/bin/bash'});
+exec('echo YEY!!');
+exec(`
+export NVM_DIR="\\$HOME/.nvm"
+[ -s "\\$NVM_DIR/nvm.sh" ] && \\\\. "\\$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install
+nvm use
+npm i -g ts-node@latest`, {shell: '/bin/bash'});
