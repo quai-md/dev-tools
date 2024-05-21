c5e6c2079452cf4bf80c19d16f2dfdfd10527761
TacB0sS <adam.vdk@nu-art-software.com>
2024-05-21 12:51:55 +0300

    test ts child process

--- .jenkins/tests/test-ts-child-process/test.ts
+++ .jenkins/tests/test-ts-child-process/test.ts
@@ -4,9 +4,24 @@ import {exec} from 'child_process';
 console.log('SUCCESS!!');
 
 exec('echo YEY!!');
-exec(`
+
+const command = `
 export NVM_DIR="\\$HOME/.nvm"
 [ -s "\\$NVM_DIR/nvm.sh" ] && \\\\. "\\$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install
 nvm use
-npm i -g ts-node@latest`, {shell: '/bin/bash'});
+npm i -g ts-node@latest`;
+
+new Promise((resolve, reject) => {
+	console.log('RUNNING!!');
+	exec(command, (error, stdout, stderr) => {
+		console.error(error);
+		resolve({stdout, stderr});
+	});
+}).then(({stdout, stderr}: any) => {
+	console.log('DONE!!');
+	console.log(stdout);
+	console.error(stderr);
+});
+
+console.log('BYE!!');
