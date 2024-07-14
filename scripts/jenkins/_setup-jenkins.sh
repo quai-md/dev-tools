af69a6bffa44e66faaa77a8bf9bcf920ed15d8cb
TacB0sS <adam.vdk@nu-art-software.com>
2024-07-14 19:30:55 +0300

    installing jre with jenkins

--- scripts/jenkins/_setup-jenkins.sh
+++ scripts/jenkins/_setup-jenkins.sh
@@ -46,6 +46,7 @@ executeCommand "sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg
 executeCommand "echo \"deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]\" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null" "Resolving Jenkins - 2"
 
 executeCommand "sudo apt-get update"
+executeCommand "sudo apt-get install fontconfig openjdk-17-jre" "Install Open JRE 17"
 executeCommand "sudo apt-get install -y jenkins" "Install Jenkins"
 executeCommand "sudo systemctl start jenkins" "Start Jenkins"
 
