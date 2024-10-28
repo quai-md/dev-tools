c2df2d0a616be2d227f384009aa736030abc1771
TacB0sS <adam.vdk@nu-art-software.com>
2024-10-28 14:44:13 +0200

    test

--- src/com/nu/art/pipeline/workflow/variables/JobParam.groovy
+++ src/com/nu/art/pipeline/workflow/variables/JobParam.groovy
@@ -1,22 +1,13 @@
 package com.nu.art.pipeline.workflow.variables
 
-class Script {
-  String script
-  Boolean sandbox = true
-
-  Script(String script, Boolean sandbox = true) {
-    this.sandbox = sandbox
-    this.script = script
-  }
-}
 
 class JobParam {
   String type
   String description
   String defaultValue
-  Script script
+  JobParamScript script
 
-  JobParam(String type, String description = "", String defaultValue = null, Script script = new Script("")) {
+  JobParam(String type, String description = "", String defaultValue = null, JobParamScript script = new JobParamScript("")) {
     this.type = type
     this.description = description
     this.defaultValue = defaultValue
