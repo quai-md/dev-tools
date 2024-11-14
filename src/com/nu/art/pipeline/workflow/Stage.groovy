95592973895b6308e1762de8c2969d1d5f5fd383
TacB0sS <adam.vdk@nu-art-software.com>
2024-11-14 15:27:21 +0200

    changed stages implementation

--- src/com/nu/art/pipeline/workflow/Stage.groovy
+++ src/com/nu/art/pipeline/workflow/Stage.groovy
@@ -3,14 +3,9 @@ package com.nu.art.pipeline.workflow
 class Stage {
   String name
   Closure toRun
-  Boolean skip
 
   Stage(String name, Closure toRun) {
     this.name = name
     this.toRun = toRun
-  }
-
-  forceSkip() {
-    this.skip = true
   }
 }
