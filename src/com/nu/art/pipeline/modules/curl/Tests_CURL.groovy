887487c6c3c4f183e11e2f723615688368b08225
TacB0sS <adam.vdk@nu-art-software.com>
2024-12-08 14:11:35 +0200

    testing curl

--- src/com/nu/art/pipeline/modules/curl/Tests_CURL.groovy
+++ src/com/nu/art/pipeline/modules/curl/Tests_CURL.groovy
@@ -15,14 +15,14 @@ class Tests_CURL
 
   TestSuite collectTests() {
     TestCase testCase1 = new TestCase("Simple", Consts.Test1_Expected, {
-      return new CURL_Request("https://my-domain.com/v1/artifacts/check-version-exists")
+      return new CURL_Request("https://my-domain.com/v1/artifacts/do-something")
         .setHeader("Content-Type", "application/json")
         .setHeader("x-application", "my-application")
         .setBody([label: "this is the body"])
         .compose("/tmp/output", "/tmp/input")
     })
     TestCase testCase2 = new TestCase("Simple", Consts.Test2_Expected, {
-      return new CURL_Request("https://my-domain.com/v1/artifacts/check-version-exists")
+      return new CURL_Request("https://my-domain.com/v1/artifacts/do-something")
         .setHeader("Content-Type", "application/json")
         .setHeader("x-application", "my-application")
         .compose("/tmp/output", "/tmp/input")
@@ -37,6 +37,6 @@ class Tests_CURL
 }
 
 class Consts {
-  public static String Test1_Expected = """curl -X GET -H "Content-Type: application/json" -H "x-application: my-application" --data @/tmp/input -o /tmp/output -s -w '%{http_code}' https://my-domain.com/v1/artifacts/check-version-exists"""
-  public static String Test2_Expected = """curl -X GET -H "Content-Type: application/json" -H "x-application: my-application" -o /tmp/output -s -w '%{http_code}' https://my-domain.com/v1/artifacts/check-version-exists"""
+  public static String Test1_Expected = """curl -X GET -H "Content-Type: application/json" -H "x-application: my-application" --data @/tmp/input -o /tmp/output -s -w '%{http_code}' https://my-domain.com/v1/do-something"""
+  public static String Test2_Expected = """curl -X GET -H "Content-Type: application/json" -H "x-application: my-application" -o /tmp/output -s -w '%{http_code}' https://my-domain.com/v1/artifacts/do-something"""
 }
