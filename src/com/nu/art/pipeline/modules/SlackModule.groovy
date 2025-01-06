af0c822606cdf207b8c8ffef10fc35060137a995
TacB0sS <adam.vdk@nu-art-software.com>
2025-01-06 11:59:38 +0200

    slack message builder

--- src/com/nu/art/pipeline/modules/SlackModule.groovy
+++ src/com/nu/art/pipeline/modules/SlackModule.groovy
@@ -1,6 +1,7 @@
 package com.nu.art.pipeline.modules
 
 import com.nu.art.pipeline.modules.build.BuildModule
+import com.nu.art.pipeline.modules.slack.SlackMessage
 import com.nu.art.pipeline.workflow.OnPipelineListener
 import com.nu.art.pipeline.workflow.Workflow
 import com.nu.art.pipeline.workflow.WorkflowModule
@@ -9,125 +10,164 @@ import com.nu.art.pipeline.workflow.variables.Var_Creds
 import com.nu.art.utils.Colors
 
 class SlackModule
-  extends WorkflowModule
-  implements OnPipelineListener {
+	extends WorkflowModule
+	implements OnPipelineListener {
 
-  private String token = "slack-token"
-  private Var_Creds SlackToken
-  private String teamDomain
-  private String onSuccess = ""
-  private String defaultChannel
-  private BuildModule buildModule
-  private boolean enabled = true
+	private String token = "slack-token"
+	private Var_Creds SlackToken
+	private String teamDomain
+	private String onSuccess = ""
+	private String defaultChannel
+	private BuildModule buildModule
+	private boolean enabled = true
 
-  @Deprecated
-  SlackModule prepare() {
-    return this
-  }
+	@Deprecated
+	SlackModule prepare() {
+		return this
+	}
 
-  void _init() {
-    setTokenCredentialsId(this.token)
-    buildModule = getModule(BuildModule.class)
-  }
+	void _init() {
+		setTokenCredentialsId(this.token)
+		buildModule = getModule(BuildModule.class)
+	}
 
-  void setToken(String token) {
-    this.token = token
-  }
+	void setToken(String token) {
+		this.token = token
+	}
 
-  void setTeam(String teamDomain) {
-    this.teamDomain = teamDomain
-  }
+	void setTeam(String teamDomain) {
+		this.teamDomain = teamDomain
+	}
 
-  void disable() {
-    this.enabled = false
-  }
+	void disable() {
+		this.enabled = false
+	}
 
-  void enableNotifications() {
-    this.enabled = true
-  }
+	void enableNotifications() {
+		this.enabled = true
+	}
 
-  void setOnSuccess(String onSuccess) {
-    this.onSuccess += "\n$onSuccess"
-  }
+	void setOnSuccess(String onSuccess) {
+		this.onSuccess += "\n$onSuccess"
+	}
 
-  void setTokenCredentialsId(String tokenCredentialId) {
-    SlackToken = new Var_Creds("string", tokenCredentialId)
-  }
+	void setTokenCredentialsId(String tokenCredentialId) {
+		SlackToken = new Var_Creds("string", tokenCredentialId)
+	}
 
-  void setDefaultChannel(String defaultChannel) {
-    this.defaultChannel = defaultChannel
-  }
+	void setDefaultChannel(String defaultChannel) {
+		this.defaultChannel = defaultChannel
+	}
 
-  void notify(GString message, String color, String channelName = defaultChannel, Boolean showTitle = true) {
-    notify(message.toString(), color, channelName, showTitle)
-  }
+	void notify(GString message, String color, String channelName = defaultChannel, Boolean showTitle = true) {
+		notify(message.toString(), color, channelName, showTitle)
+	}
 
-  void notify(String message, String color = null, String channelName = defaultChannel, Boolean showTitle = true) {
-    if (!enabled)
-      return
+	void notify(String message, String color = null, String channelName = defaultChannel, Boolean showTitle = true) {
+		if (!enabled)
+			return
 
-    String email = VarConsts.Var_User.get()
-    String preMessage = ""
-    if (showTitle) {
-      def name = buildModule.getDisplayName()
-      name = name
-        .replaceAll("&", "&amp;")
-        .replaceAll("<", "&lt;")
-        .replaceAll(">", "&gt;")
-        .replaceAll("\"", "&quot;")
-        .replaceAll("'", "&#39;")
+		String email = VarConsts.Var_User.get()
+		String preMessage = ""
+		if (showTitle) {
+			def name = buildModule.getDisplayName()
+			name = name
+				.replaceAll("&", "&amp;")
+				.replaceAll("<", "&lt;")
+				.replaceAll(">", "&gt;")
+				.replaceAll("\"", "&quot;")
+				.replaceAll("'", "&#39;")
 
-      preMessage += "<${VarConsts.Var_BuildUrl.get()}|*${name}*>"
-      preMessage += workflow.currentStage != Workflow.Stage_Started ? " after: ${buildModule.getDurationAsString()}" : ""
-      preMessage += email != null ? "\nTriggered By: *${email}*" : ""
-      preMessage += buildModule.getDescription() ? "\n${buildModule.getDescription()}" : ""
-      preMessage += "\n"
-    }
-    String finalMessage = "${preMessage}${message}"
-    finalMessage = finalMessage
-      .replaceAll(/<b>/, "*")
-      .replaceAll(/<\/b>/, "*")
-      .replaceAll(/<br>/, "\n")
-      .replaceAll(/<\/br>/, "\n")
+			preMessage += "<${VarConsts.Var_BuildUrl.get()}|*${name}*>"
+			preMessage += workflow.currentStage != Workflow.Stage_Started ? " after: ${buildModule.getDurationAsString()}" : ""
+			preMessage += email != null ? "\nTriggered By: *${email}*" : ""
+			preMessage += buildModule.getDescription() ? "\n${buildModule.getDescription()}" : ""
+			preMessage += "\n"
+		}
+		String finalMessage = "${preMessage}${message}"
+		finalMessage = finalMessage
+			.replaceAll(/<b>/, "*")
+			.replaceAll(/<\/b>/, "*")
+			.replaceAll(/<br>/, "\n")
+			.replaceAll(/<\/br>/, "\n")
 
-    workflow.script.slackSend(botUser: true, color: color, teamDomain: teamDomain, channel: channelName, message: finalMessage, tokenCredentialId: SlackToken.id)
-  }
+		workflow.script.slackSend(botUser: true, color: color, teamDomain: teamDomain, channel: channelName, message: finalMessage, tokenCredentialId: SlackToken.id)
+	}
 
-  void sendFile(String filePath, String channelName = defaultChannel, String initialComment = null) {
-    if (!enabled)
-      return
+	void sendFile(String filePath, String channelName = defaultChannel, String initialComment = null) {
+		if (!enabled)
+			return
 
-    this.logInfo("filePath: ${filePath}, channelName: ${channelName}, credentialId: ${SlackToken.id}")
-    // Assuming filePath is valid and accessible in the current workspace
-    try {
-      workflow.script.slackUploadFile(
-        initialComment: initialComment,
-        filePath: filePath,
-        channel: channelName,
-        credentialId: SlackToken.id, // Ensure this is the credential ID for a bot user token
-      )
-    } catch (e) {
-      // Handle errors appropriately
-      this.logError(e)
-    }
-  }
+		this.logInfo("filePath: ${filePath}, channelName: ${channelName}, credentialId: ${SlackToken.id}")
+		// Assuming filePath is valid and accessible in the current workspace
+		try {
+			workflow.script.slackUploadFile(
+				initialComment: initialComment,
+				filePath: filePath,
+				channel: channelName,
+				credentialId: SlackToken.id, // Ensure this is the credential ID for a bot user token
+			)
+		} catch (e) {
+			// Handle errors appropriately
+			this.logError(e)
+		}
+	}
 
-  @Override
-  void onPipelineStarted() {
-    notify("*Started*", Colors.LightGray)
-  }
+	void sendMessage(SlackMessage message) {
+		String color = message.color
+		String teamDomain = message.teamDomain ?: this.teamDomain
+		String channel = message.channel ?: this.defaultChannel
+		String messageBody = message.message ?: ""
 
-  void onPipelineAborted() {
-    notify("*Aborted* in stage: ${workflow.currentStage.name}", Colors.DarkGray)
-  }
+		messageBody
+			.replaceAll(/<b>/, "*")
+			.replaceAll(/<\/b>/, "*")
+			.replaceAll(/<br>/, "\n")
+			.replaceAll(/<\/br>/, "\n")
 
-  @Override
-  void onPipelineFailed(Throwable e) {
-    notify("*Error* in stage: ${workflow.currentStage.name}", Colors.Red)
-  }
+		workflow.script.slackSend(botUser: true, color: color, teamDomain: teamDomain, channel: channel, message: messageBody, tokenCredentialId: SlackToken.id)
+	}
 
-  @Override
-  void onPipelineSuccess() {
-    notify("*Success*${onSuccess ? "\n${onSuccess}" : ""}", Colors.Green)
-  }
+	String getLinkToJob(String label = null) {
+		if (!label)
+			label = buildModule.getDisplayName()
+
+		label = label
+			.replaceAll("&", "&amp;")
+			.replaceAll("<", "&lt;")
+			.replaceAll(">", "&gt;")
+			.replaceAll("\"", "&quot;")
+			.replaceAll("'", "&#39;")
+
+		return "<${VarConsts.Var_BuildUrl.get()}|*${label}*>"
+	}
+
+
+	String getTriggerCause() {
+		String email = VarConsts.Var_User.get()
+		return email != null ? "\nTriggered By: *${email}*" : ""
+	}
+
+	String getTimeFromStart(String label = "after") {
+		return "${label}: ${buildModule.getDurationAsString()}"
+	}
+
+	@Override
+	void onPipelineStarted() {
+		notify("*Started*", Colors.LightGray)
+	}
+
+	void onPipelineAborted() {
+		notify("*Aborted* in stage: ${workflow.currentStage.name}", Colors.DarkGray)
+	}
+
+	@Override
+	void onPipelineFailed(Throwable e) {
+		notify("*Error* in stage: ${workflow.currentStage.name}", Colors.Red)
+	}
+
+	@Override
+	void onPipelineSuccess() {
+		notify("*Success*${onSuccess ? "\n${onSuccess}" : ""}", Colors.Green)
+	}
 }
