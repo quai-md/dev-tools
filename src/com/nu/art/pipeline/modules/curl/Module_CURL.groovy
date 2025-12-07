0afe69ddd2efba2e0f3715a82e5f0e4388d265ff
TacB0sS <adam.vdk@nu-art-software.com>
2025-12-07 16:29:46 +0200

    added logs

--- src/com/nu/art/pipeline/modules/curl/Module_CURL.groovy
+++ src/com/nu/art/pipeline/modules/curl/Module_CURL.groovy
@@ -6,44 +6,46 @@ import com.nu.art.pipeline.workflow.variables.VarConsts
 import groovy.json.JsonBuilder
 
 class Module_CURL
-  extends WorkflowModule {
+	extends WorkflowModule {
 
 
-  CURL_Response execute(CURL_Request request, String responseFile = null) {
-    if (responseFile == null)
-      responseFile = "${VarConsts.Var_Workspace.get()}/tmp/curl_response_${System.currentTimeMillis()}.tmp"
+	CURL_Response execute(CURL_Request request, String responseFile = null) {
+		if (responseFile == null)
+			responseFile = "${VarConsts.Var_Workspace.get()}/tmp/curl_response_${System.currentTimeMillis()}.tmp"
 
-    String command = composeCommand(request, responseFile)
-    this.logDebug("Executing: ${command}")
+		String command = composeCommand(request, responseFile)
+		this.logDebug("Executing: ${command}")
 
-    // Execute command
-    String result = workflow.sh(command, true).trim()
-    Number responseCode = result.isInteger() ? result.toInteger() : 0
+		// Execute command
+		String result = workflow.sh(command, true).trim()
+		Number responseCode = result.isInteger() ? result.toInteger() : 0
 
-    // Parse headers (if needed, extend to capture headers with `-i` or a similar flag)
-    HashMap<String, String> responseHeaders = [:]
+		// Parse headers (if needed, extend to capture headers with `-i` or a similar flag)
+		HashMap<String, String> responseHeaders = [:]
 
-    return new CURL_Response(responseFile, responseHeaders, responseCode)
-  }
+		return new CURL_Response(responseFile, responseHeaders, responseCode)
+	}
 
-  String composeCommand(CURL_Request request, String pathToResponseFile, String pathToBodyFile = "${VarConsts.Var_Workspace.get()}/tmp/curl_body_${System.currentTimeMillis()}.tmp") {
-    if (!request.url)
-      throw new BadImplementationException("URL is required for the request.")
+	String composeCommand(CURL_Request request, String pathToResponseFile, String pathToBodyFile = "${VarConsts.Var_Workspace.get()}/tmp/curl_body_${System.currentTimeMillis()}.tmp") {
+		if (!request.url)
+			throw new BadImplementationException("URL is required for the request.")
 
-    // Prepare headers
-    String headerOptions = request.headers.collect { key, value -> "-H \"${key}: ${value}\"" }.join(" ")
+		// Prepare headers
+		String headerOptions = request.headers.collect { key, value -> "-H \"${key}: ${value}\"" }.join(" ")
 
-    // Prepare body
-    String bodyOption = ""
-    Object body = request.body
-    if (body) {
-      workflow.writeToFile(pathToBodyFile, body instanceof String ? body : new JsonBuilder(body).toString())
-      bodyOption = " --data @${pathToBodyFile}"
-    }
+		// Prepare body
+		String bodyOption = ""
+		Object body = request.body
+		if (body) {
+			def finalBody = body instanceof String ? body : new JsonBuilder(body).toString()
+			workflow.writeToFile(pathToBodyFile, finalBody)
+			bodyOption = " --data @${pathToBodyFile}"
+			this.logInfo("finalBody: ${finalBody}")
+		}
 
-    // Construct curl command
-    String command = "curl --http1.1 -v -X ${request.method} ${headerOptions}${bodyOption} -o ${pathToResponseFile} -s -w '%{http_code}' ${request.url}"
-    command
-  }
+		// Construct curl command
+		String command = "curl --http1.1 -v -X ${request.method} ${headerOptions}${bodyOption} -o ${pathToResponseFile} -s -w '%{http_code}' ${request.url}"
+		command
+	}
 }
 
