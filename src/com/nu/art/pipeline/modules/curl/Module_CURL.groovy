edfb23f1f7416292c4f0699efdd50cb7bcdd428c
TacB0sS <adam.vdk@nu-art-software.com>
2025-12-07 18:16:44 +0200

    attempt http1.1

--- src/com/nu/art/pipeline/modules/curl/Module_CURL.groovy
+++ src/com/nu/art/pipeline/modules/curl/Module_CURL.groovy
@@ -51,7 +51,7 @@ class Module_CURL
 		}
 
 		// Construct curl command
-		String command = "./curl-amd64 -v -X ${request.method} ${bodyOption} -o ${pathToResponseFile} ${headerOptions} -s -w '%{http_code}' ${request.url}"
+		String command = "./curl-amd64 --http1.1 -v -X ${request.method} ${bodyOption} -o ${pathToResponseFile} ${headerOptions} -s -w '%{http_code}' ${request.url}"
 		command
 	}
 }
