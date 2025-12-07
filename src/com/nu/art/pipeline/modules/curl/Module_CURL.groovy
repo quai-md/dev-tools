3ef1256dc40d5c0cf142caf36f5d95d57d3bb981
TacB0sS <adam.vdk@nu-art-software.com>
2025-12-07 16:47:17 +0200

    logs

--- src/com/nu/art/pipeline/modules/curl/Module_CURL.groovy
+++ src/com/nu/art/pipeline/modules/curl/Module_CURL.groovy
@@ -44,8 +44,7 @@ class Module_CURL
 		}
 
 		// Construct curl command
-		String command = "curl --http1.1 -v -X ${request.method} ${headerOptions}${bodyOption} -o ${pathToResponseFile} -s -w '%{http_code}' ${request.url}"
+		String command = "curl -v -X ${request.method} ${headerOptions}${bodyOption} -o ${pathToResponseFile} -s -w '%{http_code}' ${request.url}"
 		command
 	}
 }
-
