ca28d867bba52f644e815587911cca139a779d15
TacB0sS <adam.vdk@nu-art-software.com>
2026-01-21 15:35:41 +0200

    cxreating ssh files

--- src/com/nu/art/pipeline/modules/build/BuildModule.groovy
+++ src/com/nu/art/pipeline/modules/build/BuildModule.groovy
@@ -1,7 +1,6 @@
 package com.nu.art.pipeline.modules.build
 
 import com.cloudbees.groovy.cps.NonCPS
-import com.nu.art.pipeline.workflow.Workflow
 import com.nu.art.pipeline.workflow.WorkflowModule
 import com.nu.art.pipeline.workflow.variables.VarConsts
 import com.nu.art.pipeline.workflow.variables.Var_Creds
@@ -14,149 +13,151 @@ import org.jenkinsci.plugins.pipeline.utility.steps.fs.FileWrapper
 import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 
 public class BuildModule
-  extends WorkflowModule {
+	extends WorkflowModule {
 
-  TriggerCause[] triggers
+	TriggerCause[] triggers
 
-  @Override
-  void _init() {
-    Run build = workflow.getCurrentBuild().rawBuild
-    List<Cause> causes = build.getCauses()
-    triggers = causes.collect { new TriggerCause(it) }
-    triggers.each {
-      if (it.type == TriggerCause.Type_Unknown)
-        logWarning("Missing case: ${it.print()}")
-      else
-        logInfo("${it.print()}")
-    }
-  }
+	@Override
+	void _init() {
+		Run build = workflow.getCurrentBuild().rawBuild
+		List<Cause> causes = build.getCauses()
+		triggers = causes.collect { new TriggerCause(it) }
+		triggers.each {
+			if (it.type == TriggerCause.Type_Unknown)
+				logWarning("Missing case: ${it.print()}")
+			else
+				logInfo("${it.print()}")
+		}
+	}
 
-  void printCauses() {
-    Run build = workflow.getCurrentBuild().rawBuild
-    List<Cause> causes = build.getCauses()
-    causes.forEach { logInfo("scm: ${it}") }
-  }
+	void printCauses() {
+		Run build = workflow.getCurrentBuild().rawBuild
+		List<Cause> causes = build.getCauses()
+		causes.forEach { logInfo("scm: ${it}") }
+	}
 
-  TriggerCause[] getTriggerCause(String causeType) {
-    return triggers.findAll { it.type == causeType }
-  }
+	TriggerCause[] getTriggerCause(String causeType) {
+		return triggers.findAll { it.type == causeType }
+	}
 
-  boolean getUser() {
-    if (userCause != null)
-      return userCause.userName
+	boolean getUser() {
+		if (userCause != null)
+			return userCause.userName
 
-    if (scmCause != null)
-      if (scmCause.getClass().getSimpleName() == "GitHubPushCause")
-        return scmUser = scmCause.pushedBy
+		if (scmCause != null)
+			if (scmCause.getClass().getSimpleName() == "GitHubPushCause")
+				return scmUser = scmCause.pushedBy
 
-    return "UNKNOWN"
-  }
+		return "UNKNOWN"
+	}
 
-  boolean getscm() {
-    scmCause.getShortDescription()
-    return scmCause != null
+	boolean getscm() {
+		scmCause.getShortDescription()
+		return scmCause != null
 
-  }
+	}
 
-  void setDisplayName(String displayName) {
-    logInfo("Setting display name: ${displayName}")
-    workflow.getCurrentBuild().displayName = displayName
-  }
+	void setDisplayName(String displayName) {
+		logInfo("Setting display name: ${displayName}")
+		workflow.getCurrentBuild().displayName = displayName
+	}
 
-  String getDisplayName() {
-    return workflow.getCurrentBuild().displayName
-  }
+	String getDisplayName() {
+		return workflow.getCurrentBuild().displayName
+	}
 
-  void setDescription(String description) {
-    logInfo("Setting description: ${description}")
-    workflow.getCurrentBuild().description = description
-  }
+	void setDescription(String description) {
+		logInfo("Setting description: ${description}")
+		workflow.getCurrentBuild().description = description
+	}
 
-  String getDescription() {
-    return workflow.getCurrentBuild().description
-  }
+	String getDescription() {
+		return workflow.getCurrentBuild().description
+	}
 
-  void setResult(String result) {
-    workflow.getCurrentBuild().result = result
-  }
+	void setResult(String result) {
+		workflow.getCurrentBuild().result = result
+	}
 
-  String getResult() {
-    return workflow.getCurrentBuild().result
-  }
+	String getResult() {
+		return workflow.getCurrentBuild().result
+	}
 
-  String getCurrentResult() {
-    return workflow.getCurrentBuild().currentResult
-  }
+	String getCurrentResult() {
+		return workflow.getCurrentBuild().currentResult
+	}
 
-  String getDurationAsString() {
-    return workflow.getCurrentBuild().durationString.replaceAll("and counting", "")
-  }
+	String getDurationAsString() {
+		return workflow.getCurrentBuild().durationString.replaceAll("and counting", "")
+	}
 
 
-  String collectDetails() {
-    String displayName = getDisplayName() ? "\ndisplayName: ${getDisplayName()}" : ""
-    String description = getDescription() ? "\ndescription: ${getDescription()}" : ""
-    String currentResult = getCurrentResult() ? "\ncurrentResult: ${getCurrentResult()}" : ""
+	String collectDetails() {
+		String displayName = getDisplayName() ? "\ndisplayName: ${getDisplayName()}" : ""
+		String description = getDescription() ? "\ndescription: ${getDescription()}" : ""
+		String currentResult = getCurrentResult() ? "\ncurrentResult: ${getCurrentResult()}" : ""
 
-    return displayName + description + currentResult + result
-  }
+		return displayName + description + currentResult + result
+	}
 
-  @NonCPS
-  String getTestStatuses() {
-    AbstractTestResultAction testResultAction = workflow.getCurrentBuild().rawBuild.getAction(AbstractTestResultAction.class)
-    if (testResultAction == null)
-      return "Could not find tests"
+	@NonCPS
+	String getTestStatuses() {
+		AbstractTestResultAction testResultAction = workflow.getCurrentBuild().rawBuild.getAction(AbstractTestResultAction.class)
+		if (testResultAction == null)
+			return "Could not find tests"
 
-    def total = testResultAction.totalCount
-    def failed = testResultAction.failCount
-    def skipped = testResultAction.skipCount
-    def passed = total - failed - skipped
+		def total = testResultAction.totalCount
+		def failed = testResultAction.failCount
+		def skipped = testResultAction.skipCount
+		def passed = total - failed - skipped
 
-    return "Test Status:\n  Passed: *${passed}*, Failed: *${failed} ${testResultAction.failureDiffString}*, Skipped: *${skipped}*".toString()
-  }
+		return "Test Status:\n  Passed: *${passed}*, Failed: *${failed} ${testResultAction.failureDiffString}*, Skipped: *${skipped}*".toString()
+	}
 
-  String pathToFile(String pathToFile) {
-    return "${VarConsts.Var_Workspace.get()}/${pathToFile}"
-  }
+	String pathToFile(String pathToFile) {
+		return "${VarConsts.Var_Workspace.get()}/${pathToFile}"
+	}
 
-  String pathToFileImpl(String pathToFile, RunWrapper build) {
-    return "${VarConsts.Var_JenkinsHome.get()}/jobs/${VarConsts.Var_JobName.get()}/builds/${build.getNumber()}/${pathToFile}"
-  }
+	String pathToFileImpl(String pathToFile, RunWrapper build) {
+		return "${VarConsts.Var_JenkinsHome.get()}/jobs/${VarConsts.Var_JobName.get()}/builds/${build.getNumber()}/${pathToFile}"
+	}
 
-  CopyArtifacts copyArtifacts(String name, int build) {
-    return new CopyArtifacts(this).job(name, build)
-  }
+	CopyArtifacts copyArtifacts(String name, int build) {
+		return new CopyArtifacts(this).job(name, build)
+	}
 
-  FileWrapper[] findFiles(String filter) {
-    return workflow.script.findFiles(glob: filter)
-  }
+	FileWrapper[] findFiles(String filter) {
+		return workflow.script.findFiles(glob: filter)
+	}
 
-  RunWrapper getLastSuccessfulBuild() {
-    workflow.getCurrentBuild().getPreviousSuccessfulBuild()
-  }
+	RunWrapper getLastSuccessfulBuild() {
+		workflow.getCurrentBuild().getPreviousSuccessfulBuild()
+	}
 
-  JobTrigger triggerJob(String name) {
-    return new JobTrigger(workflow, name)
-  }
+	JobTrigger triggerJob(String name) {
+		return new JobTrigger(workflow, name)
+	}
 
-  void writeToFile(String toFile, Object fileContent) {
-    workflow.script.writeFile file: toFile, text: fileContent
-  }
+	void writeToFile(String toFile, Object fileContent) {
+		workflow.script.writeFile file: toFile, text: fileContent
+	}
 
-  void readFromFile(String pathToFile) {
-    def content = workflow.script.readFile file: pathToFile
-    return content
-  }
+	void readFromFile(String pathToFile) {
+		def content = workflow.script.readFile file: pathToFile
+		return content
+	}
 
-  void setSecretAsSSH_Key(String secretId) {
-    def SSH_KEY = new Var_Env("GIT_SSH_KEY")
-    Var_Creds[] sshKeyCreds = [new Var_CredsFile("sshUserPrivateKey", secretId, SSH_KEY)]
-    workflow.withCredentials(sshKeyCreds, {
-      def pathToSSHFile = "~/.ssh/id_rsa"
-      sh("cp ${SSH_KEY.get()} ${pathToSSHFile}")
-      sh("chmod 600 ${pathToSSHFile}")
-      sh("echo \"Host *\" >> ~/.ssh/config")
-      sh("echo \"  StrictHostKeyChecking no\" >> ~/.ssh/config")
-    })
-  }
+	void setSecretAsSSH_Key(String secretId) {
+		def SSH_KEY = new Var_Env("GIT_SSH_KEY")
+		Var_Creds[] sshKeyCreds = [new Var_CredsFile("sshUserPrivateKey", secretId, SSH_KEY)]
+		workflow.withCredentials(sshKeyCreds, {
+			def pathToSSHFile = "~/.ssh/id_rsa"
+			sh("[ ! -d /home/jenkins/.ssh ] && mkdir /home/jenkins/.ssh")
+			sh("chmod 700 /home/jenkins/.ssh")
+			sh("cp ${SSH_KEY.get()} ${pathToSSHFile}")
+			sh("chmod 600 ${pathToSSHFile}")
+			sh("echo \"Host *\" >> ~/.ssh/config")
+			sh("echo \"  StrictHostKeyChecking no\" >> ~/.ssh/config")
+		})
+	}
 }
